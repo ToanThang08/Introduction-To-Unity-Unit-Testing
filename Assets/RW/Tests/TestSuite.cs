3c99116958c6a9608c888afaa27833756e93f82e
Toan Thang <box.dota@gmail.com>
2023-08-23 18:03:51 +0300

    Writing Your First Unit Test

--- Assets/RW/Tests/TestSuite.cs
+++ Assets/RW/Tests/TestSuite.cs
@@ -6,20 +6,24 @@ using UnityEngine.TestTools;
 
 public class TestSuite
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void TestSuiteSimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
+    private Game game;
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
+    //1
     [UnityTest]
-    public IEnumerator TestSuiteWithEnumeratorPasses()
+    public IEnumerator AsteroidsMoveDown()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        //2
+        GameObject gameGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+        //3
+        GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+        //4
+        float initialYPos = asteroid.transform.position.y;
+        //5
+        yield return new WaitForSeconds(0.1f);
+        //6
+        Assert.Less(asteroid.transform.position.y, initialYPos);
+        //7
+        Object.Destroy(game.gameObject);
     }
 }
