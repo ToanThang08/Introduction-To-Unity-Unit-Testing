9f65e9e050e1513464b195ddb02ec3d4ff966dee
Toan Thang <box.dota@gmail.com>
2023-08-23 18:23:24 +0300

    Setting Up and Tearing Down Phases

--- Assets/RW/Tests/TestSuite.cs
+++ Assets/RW/Tests/TestSuite.cs
@@ -8,6 +8,21 @@ public class TestSuite
 {
     private Game game;
 
+    [SetUp]
+    public void Setup()
+    {
+        GameObject gameGameObject =
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(game.gameObject);
+    }
+
+
     //1
     [UnityTest]
     public IEnumerator AsteroidsMoveDown()
